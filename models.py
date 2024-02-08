fb026df47c30716682e6a691aea6312b9174ee61
Santhosh G S <santhoshgs013@gmail.com>
2024-02-08 23:47:16 +0530

    Working on Profile page

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +11,9 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     passhash = db.Column(db.String(256), nullable=False)
     name = db.Column(db.String(120), nullable=False)
+    requests = db.relationship("Request", backref="user", lazy=True)
+    borrows = db.relationship("Borrow", backref="user", lazy=True)
+
 
 class Librarian(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,41 +22,55 @@ class Librarian(db.Model):
     passhash = db.Column(db.String(256), nullable=False)
     name = db.Column(db.String(120), nullable=False)
 
+
 class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False)
     image = db.Column(db.LargeBinary, nullable=True)
     description = db.Column(db.String(1024), nullable=False)
-    books = db.relationship('Book', backref='genre', lazy=True)
+    books = db.relationship("Book", backref="genre", lazy=True)
+
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=True, nullable=False)
     authors = db.Column(db.String(120), nullable=False)
-    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'), nullable=False)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"), nullable=False)
     image = db.Column(db.LargeBinary, nullable=True)
     content = db.Column(db.LargeBinary, nullable=False)
-    genre = db.relationship('Genre', backref='book', lazy=True)
-    
+    requests = db.relationship("Request", backref="book", lazy=True)
+    borrows = db.relationship("Borrow", backref="book", lazy=True)
+
+
 class Request(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable=False)
     date_requested = db.Column(db.DateTime, nullable=False)
     days_requested = db.Column(db.Integer, nullable=False)
-    user = db.relationship('User', backref='request', lazy=True)
-    book = db.relationship('Book', backref='request', lazy=True)
+
 
 class Borrow(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable=False)
     date_issued = db.Column(db.DateTime, nullable=False)
     date_due = db.Column(db.DateTime, nullable=False)
     remaining_days = db.Column(db.Integer, nullable=False)
-    user = db.relationship('User', backref='borrow', lazy=True)
+
 
 with app.app_context():
     db.create_all()
+
+    is_librarian_present = db.session.query(Librarian.id).first() is not None
+    if not is_librarian_present:
+        librarian = Librarian(
+            username="librarian",
+            email="librarian@email.com",
+            passhash=generate_password_hash("1234"),
+            name="Librarian",
+        )
+        db.session.add(librarian)
+        db.session.commit()
+        print("Librarian created")
--- routes.py
+++ routes.py
@@ -1,12 +1,30 @@
 import re
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, session
 from email_validator import validate_email
 from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 from models import User, Librarian, Genre, Book, Request, Borrow, db
 from app import app
 
+# --------------- Decorators ---------------
+
+
+def auth_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if "user_id" not in session:
+            flash("You need to login or register to access this page")
+            return redirect(url_for("login"))
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+# ----------------- Routes -----------------
+
 
 @app.route("/")
+@auth_required
 def index():
     return render_template("index.html")
 
@@ -71,3 +89,151 @@ def register_post():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    username_email = request.form.get("username-email")
+    password = request.form.get("password")
+
+    if not username_email or not password:
+        flash("Please fill all the fields")
+        return redirect(url_for("login"))
+
+    user = User.query.filter_by(username=username_email).first()
+    if user is None:
+        user = User.query.filter_by(email=username_email).first()
+    librarian = Librarian.query.filter_by(username=username_email).first()
+    if librarian is None:
+        librarian = Librarian.query.filter_by(email=username_email).first()
+
+    if user is None and librarian is None:
+        flash("Username or email does not exist")
+        return redirect(url_for("login"))
+
+    if user:
+        if not check_password_hash(user.passhash, password):
+            flash("Incorrect password")
+            return redirect(url_for("login"))
+    else:
+        if not check_password_hash(librarian.passhash, password):
+            flash("Incorrect password")
+            return redirect(url_for("login"))
+
+    session["user_id"] = user.id if user else librarian.id
+    return redirect(url_for("index"))
+
+
+@app.route("/profile")
+@auth_required
+def profile():
+    user = User.query.get(session["user_id"])
+    librarian = Librarian.query.get(session["user_id"])
+    return render_template("profile.html", user=user, librarian=librarian)
+
+
+@app.route("/profile", methods=["POST"])
+@auth_required
+def profile_update():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    username = request.form.get("username")
+
+    if not name or not email or not username:
+        flash("Please fill all the fields")
+        return redirect(url_for("profile"))
+
+    user = User.query.get(session["user_id"])
+    librarian = Librarian.query.get(session["user_id"])
+
+    if user:
+        if user.username != username:
+            new_username = User.query.filter_by(username=username).first()
+            if new_username:
+                flash("Username already exists")
+                return redirect(url_for("profile"))
+        if user.email != email:
+            new_email = User.query.filter_by(email=email).first()
+            if new_email:
+                flash("Email already exists")
+                return redirect(url_for("profile"))
+        user.name = name
+        user.email = email
+        user.username = username
+    else:
+        if librarian.username != username:
+            new_username = Librarian.query.filter_by(username=username).first()
+            if new_username:
+                flash("Username already exists")
+                return redirect(url_for("profile"))
+        if librarian.email != email:
+            new_email = Librarian.query.filter_by(email=email).first()
+            if new_email:
+                flash("Email already exists")
+                return redirect(url_for("profile"))
+        librarian.name = name
+        librarian.email = email
+        librarian.username = username
+
+    db.session.commit()
+    flash("Profile updated successfully")
+    return redirect(url_for("profile"))
+
+
+@app.route("/profile/change-password")
+@auth_required
+def change_password():
+    return render_template("change-password.html")
+
+
+@app.route("/profile/change-password", methods=["POST"])
+@auth_required
+def change_password_post():
+    cpassword = request.form.get("cpassword")
+    npassword = request.form.get("npassword")
+    rnpassword = request.form.get("rnpassword")
+
+    if not cpassword or not npassword or not rnpassword:
+        flash("Please fill all the fields")
+        return redirect(url_for("change_password"))
+
+    user = User.query.get(session["user_id"])
+    librarian = Librarian.query.get(session["user_id"])
+
+    if npassword != rnpassword:
+        flash("New passwords do not match")
+        return redirect(url_for("change_password"))
+    if len(npassword) < 8:
+        flash("New password should be at least 8 characters long")
+        return redirect(url_for("change_password"))
+    if not re.search(r"[A-Z]", npassword):
+        flash("New password should contain at least one uppercase letter")
+        return redirect(url_for("change_password"))
+    if not re.search(r"[a-z]", npassword):
+        flash("New password should contain at least one lowercase letter")
+        return redirect(url_for("change_password"))
+    if not re.search(r"\d", npassword):
+        flash("New password should contain at least one digit")
+        return redirect(url_for("change_password"))
+
+    if user:
+        if user.passhash != check_password_hash(cpassword):
+            flash("Incorrect password")
+            return redirect(url_for("change_password"))
+        user.passhash = generate_password_hash(npassword)
+    else:
+        if librarian.passhash != check_password_hash(cpassword):
+            flash("Incorrect password")
+            return redirect(url_for("change_password"))
+        librarian.passhash = generate_password_hash(npassword)
+
+    db.session.commit()
+    flash("Password changed successfully")
+    return redirect(url_for("profile"))
+
+
+@app.route("/logout")
+@auth_required
+def logout():
+    session.pop("user_id")
+    return redirect(url_for("login"))
